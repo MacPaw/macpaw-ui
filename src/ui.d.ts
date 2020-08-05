d5120c8f3e2cb137a3823b35d779fe15c5248c10
korovka <neporozhniy@gmail.com>
2020-08-05 15:50:17 +0300

    fix export

--- src/ui.d.ts
+++ src/ui.d.ts
@@ -1,6 +1,12 @@
 declare module 'macpaw-ui';
 
+export { default as Badge } from './Badge/Badge';
+export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
 export { default as Button } from './Button/Button';
+export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as FormRow } from './FormRow/FormRow';
+export { default as Hint } from './Hint/Hint';
 export { default as Input } from './Input/Input';
 export { default as Loader } from './Loader/Loader';
-export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
+export { default as Panel } from './Panel/Panel';
+export { default as Select } from './Select/Select';
--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,10 @@
+export { default as Badge } from './Badge/Badge';
+export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
 export { default as Button } from './Button/Button';
+export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as FormRow } from './FormRow/FormRow';
+export { default as Hint } from './Hint/Hint';
 export { default as Input } from './Input/Input';
 export { default as Loader } from './Loader/Loader';
-export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
+export { default as Panel } from './Panel/Panel';
+export { default as Select } from './Select/Select';
