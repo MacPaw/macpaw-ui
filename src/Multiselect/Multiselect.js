655d6396843e756b52affb0d3a2059fe6746b82c
Borys Zahorodnii <boriszagorodniy@hotmail.com>
2021-02-08 10:40:28 +0200

    linter

--- src/Multiselect/Multiselect.js
+++ src/Multiselect/Multiselect.js
@@ -4,60 +4,6 @@ import Select from '../Select/Select';
 import Tag from '../Tag/Tag';
 import Hint from '../Hint/Hint';
 
-// const Multiselect = (props) => {
-//   const {
-//     className,
-//     children,
-//     placeholder,
-//     onChange,
-
-//     size,
-//     errorMessage,
-//     ...other
-//   } = props;
-//   const options = React.Children.map(children, c => ({
-//     value: c.props.value,
-//     label: c.props.children,
-//     selected: Boolean(c.props.selected),
-//   }));
-//   const selectedOptions = options.filter(o => o.selected);
-//   const selectedValues = selectedOptions.map(o => o.value);
-//   const rootClassnames = cx('multiselect', { className, '-error': Boolean(errorMessage), '-empty': !selectedOptions.length, '-medium': size === 'medium', '-small': size === 'small' });
-
-//   function toggleSelected(toggleOption) {
-//     if (onChange) {
-//       if (toggleOption.selected) {
-//         onChange(selectedValues.filter(v => v !== toggleOption.value));
-//       } else {
-//         onChange([...selectedValues, toggleOption.value]);
-//       }
-//     }
-//   }
-
-//   return (
-//     <label>
-//       {/* {label && <span className="h6">{label}</span>} */}
-//       <div className={rootClassnames} {...other}>
-//         {selectedOptions.map(o => (
-//           <Tag key={o.value} onRemove={() => toggleSelected(o)}>
-//             {o.label}
-//           </Tag>
-//         ))}
-
-//         <Select value="" onChange={event => toggleSelected(options.find(o => o.value === event.target.value))}>
-//           <option value="" disabled>{placeholder || ''}</option>
-//           {options.map(o => (
-//             <option key={o.value} value={o.value}>{o.label}</option>
-//           ))}
-//         </Select>
-//       </div>
-//       {errorMessage && (<Hint style={{ marginTop: 6 }} error>{`* ${errorMessage}`}</Hint>)}
-//     </label>
-//   );
-// };
-
-// export default Multiselect;
-
 const Multiselect = (props) => {
   const {
     className,
@@ -69,13 +15,13 @@ const Multiselect = (props) => {
     label,
     ...other
   } = props;
-  const options = React.Children.map(children, (c) => ({
+  const options = React.Children.map(children, c => ({
     value: c.props.value,
     label: c.props.children,
     selected: Boolean(c.props.selected),
   }));
-  const selectedOptions = options.filter((o) => o.selected);
-  const selectedValues = selectedOptions.map((o) => o.value);
+  const selectedOptions = options.filter(o => o.selected);
+  const selectedValues = selectedOptions.map(o => o.value);
   const rootClassnames = cx(
     'multiselect',
     !selectedOptions.length && '-empty',
@@ -88,7 +34,7 @@ const Multiselect = (props) => {
   function toggleSelected(toggleOption) {
     if (onChange) {
       if (toggleOption.selected) {
-        onChange(selectedValues.filter((v) => v !== toggleOption.value));
+        onChange(selectedValues.filter(v => v !== toggleOption.value));
       } else {
         onChange([...selectedValues, toggleOption.value]);
       }
@@ -96,11 +42,11 @@ const Multiselect = (props) => {
   }
 
   return (
-    <>
+    <div className="multiselect-wrapper">
       {label && <span className="h6">{label}</span>}
       <div className={rootClassnames} {...other}>
         <div className='tags'>
-          {selectedOptions.map((o) => (
+          {selectedOptions.map(o => (
             <Tag key={o.value} onRemove={() => toggleSelected(o)}>
               {o.label}
             </Tag>
@@ -110,13 +56,13 @@ const Multiselect = (props) => {
         <Select
           value=''
           onChange={(event) =>
-            toggleSelected(options.find((o) => o.value === event.target.value))
+            toggleSelected(options.find(o => o.value === event.target.value))
           }
         >
           <option value='' disabled>
             {placeholder || ''}
           </option>
-          {options.map((o) => (
+          {options.map(o => (
             <option key={o.value} value={o.value}>
               {o.label}
             </option>
@@ -124,7 +70,7 @@ const Multiselect = (props) => {
         </Select>
       </div>
       {errorMessage && <Hint style={{ marginTop: 6 }} error>{`* ${errorMessage}`}</Hint>}
-    </>
+    </div>
   );
 };
 
