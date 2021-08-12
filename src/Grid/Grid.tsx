2d996363fc545ad7862eb587b81cd5b1bbe8b476
korovka <neporozhniy@gmail.com>
2021-08-12 13:58:35 +0300

    fix: grid layout

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -11,20 +11,15 @@ const Grid: React.FC<Grid> = (props) => {
   const { icon, action, notification, children, className, ...other } = props;
   let gridClassNames = 'grid';
   if (className) gridClassNames += ` ${className}`;
-  const rowsCount = React.Children.count(children);
-  let gridActionClassNames = 'grid-action';
-  let gridIconClassNames = 'grid-icon';
-  if (rowsCount > 1) gridActionClassNames += ' -top';
-  if (rowsCount > 1) gridIconClassNames += ' -top';
 
   return (
     <Panel {...other} className={gridClassNames}>
       <div className="grid-layout">
-        {icon && <div className={gridIconClassNames}>{icon}</div>}
+        {icon && <div className="grid-icon">{icon}</div>}
         <div className="grid-rows">
           {children}
         </div>
-        <div className={gridActionClassNames}>{action}</div>
+        <div className="grid-action">{action}</div>
       </div>
       {notification && <div className="grid-notification">{notification}</div>}
     </Panel>
