ad391e7d485e84d47121bfc39f222536743feccb
korovka <neporozhniy@gmail.com>
2021-09-02 14:39:33 +0300

    handle checkbox fontsize

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -8,15 +8,10 @@ export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
-  const {
-    className,
-    error,
-    children,
-    ...other
-  } = props;
+  const { className, error, children, style, ...other } = props;
 
   return (
-    <label className={cx('checkbox', className, error && '-error')}>
+    <label className={cx('checkbox', className, error && '-error')} style={style}>
       <input type="checkbox" {...other} ref={ref} />
       <span />
       {children}
