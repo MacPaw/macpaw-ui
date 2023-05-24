1a4517ac9086fcde487c07d40034920b7ef39d2f
Oleksandr Holovchenko <algolj@macpaw.com>
2023-05-24 12:30:59 +0300

    chore: added new functionality for Tooltip

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cx from 'clsx';
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  limitShift,
+  useFocus,
+  useClick,
+  useHover,
+  useInteractions,
+  useTransitionStyles,
+  FloatingArrow,
+  arrow,
+  safePolygon,
+} from '@floating-ui/react';
 
 interface Tooltip {
   content: React.ReactNode;
@@ -7,6 +23,7 @@ interface Tooltip {
   maxWidth?: number | string;
   forceShow?: boolean;
   forceHide?: boolean;
+  openOnСlick?: boolean;
 }
 
 const Tooltip: React.FC<React.PropsWithChildren<Tooltip>> = ({
@@ -14,29 +31,108 @@ const Tooltip: React.FC<React.PropsWithChildren<Tooltip>> = ({
   content,
   position,
   maxWidth,
-  forceShow = false,
-  forceHide = false,
+  forceShow,
+  forceHide,
+  openOnСlick,
 }) => {
-  const messageStyles = maxWidth ? ({ width: maxWidth } as React.CSSProperties) : {};
-  const messageClassNames = cx(
-    'tooltip-message',
-    `-${position}`,
-    {
-      '-show': forceShow,
-      '-hide': forceHide,
-      '-custom-width': !!maxWidth,
-    }
-  );
+  const messageStyles = maxWidth
+    ? ({ width: maxWidth } as React.CSSProperties)
+    : {};
+
+  const arrowRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(Boolean(forceShow && !forceHide));
+  }, [forceShow, forceHide]);
+
+  const { middlewareData, refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: forceShow || forceHide ? undefined : setIsOpen,
+    placement: position,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(8),
+      arrow({
+        element: arrowRef,
+      }),
+      flip(),
+      shift({
+        limiter: limitShift(),
+      }),
+    ],
+  });
+
+  const { styles } = useTransitionStyles(context, {
+    duration: 400,
+  });
+
+  const getArrowPosition = () => {
+    const shiftPosition
+      = middlewareData?.shift?.y || middlewareData?.shift?.x || 0;
+    const arrowPosition
+      = middlewareData?.arrow?.y || middlewareData?.arrow?.x || 0;
+    const coordinates = arrowPosition - shiftPosition;
+
+    const overflowSize = (arrowPosition * 2) / shiftPosition;
+
+    const positionOnOverflow = `${middlewareData?.shift?.x ? '85' : '59'}%`;
+
+    return coordinates < 10
+      ? 10
+      : middlewareData?.flip?.overflows?.[0]
+        && overflowSize > -3
+        && shiftPosition < 0
+        ? positionOnOverflow
+        : coordinates;
+  };
+
+  const hover = useHover(context, {
+    enabled: !openOnСlick,
+    handleClose: safePolygon({
+      requireIntent: false,
+    }),
+  });
+  const focus = useFocus(context);
+  const click = useClick(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    hover,
+    focus,
+    openOnСlick ? click : undefined,
+  ]);
 
   return (
-    <div className="tooltip">
-      <div className={messageClassNames}>
-        <div className="tooltip-content" style={messageStyles}>{content}</div>
-      </div>
-      <div className="tooltip-trigger">
+    <>
+      <div
+        className="tooltip-trigger"
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
         {children}
       </div>
-    </div>
+      {isOpen && (
+        <div ref={refs.setFloating} style={{ ...floatingStyles, ...styles }}>
+          <FloatingArrow
+            ref={arrowRef}
+            context={context}
+            tipRadius={2}
+            height={9}
+            width={18}
+            staticOffset={getArrowPosition()}
+          />
+          <div
+            className={cx('tooltip-content tooltip-message', {
+              '-custom-width': !!maxWidth,
+            })}
+            style={messageStyles}
+            {...getFloatingProps()}
+          >
+            {content}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
